052531b3b12d1390eb0c0d4e877e4a494cbe07c8
batistta <assuerio12@gmail.com>
2020-08-20 21:04:25 -0300

    initial commit

---
+++ multiplicacao recursiv/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplicacao_recursiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+            Console.WriteLine("digite 1 numero: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("digite 1 numero: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine(Multiplicar(a, b));
+            Console.ReadKey();
+        }
+        static int Multiplicar(int A, int B)
+        {
+            int MultAux(int a, int aux, int b)
+            {
+                if (a == 0 || b == 0)
+                    return 0;
+                if (b == 1)
+                    return a;
+                a += aux;
+                return MultAux(a, aux, b - 1);
+            }
+            return MultAux(A, A, B);
+        }
+    }
+}
